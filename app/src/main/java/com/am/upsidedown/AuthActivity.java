68d811b75700dec599b5d9f129cebcdf1351e40c
Arbena <arbena.musa@student.uni-pr.edu>
2020-05-13 16:29:10 +0200

    Added ViewPagerAdapter for tablayout's fragments

--- app/src/main/java/com/am/upsidedown/AuthActivity.java
+++ app/src/main/java/com/am/upsidedown/AuthActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.am.upsidedown.auth.LogInFragment;
+import com.am.upsidedown.auth.SignUpFragment;
+import com.am.upsidedown.utils.ViewPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
 
 public class AuthActivity extends AppCompatActivity {
@@ -19,6 +23,16 @@ public class AuthActivity extends AppCompatActivity {
         logTabs = (TabLayout) findViewById(R.id.logTabs);
         authViewPager = (ViewPager) findViewById(R.id.logViewpager);
 
+        setupViewPager(authViewPager);
         logTabs.setupWithViewPager(authViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        //The Fragment Manager provides the methods used to access the Fragments currently added to the
+        //Activity, and to perform Fragment Transactions to add, remove, and replace Fragments.
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new LogInFragment(), "Log In");
+        adapter.addFragment(new SignUpFragment(), "Sign Up");
+        viewPager.setAdapter(adapter);
     }
 }
