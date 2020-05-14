6da1e87a2ed202d23d944be3e299b7d57f76f059
Arbena <arbena.musa@student.uni-pr.edu>
2020-05-14 10:40:31 +0200

    Added side menu on MainActivity's layout

--- app/src/main/java/com/am/upsidedown/MainActivity.java
+++ app/src/main/java/com/am/upsidedown/MainActivity.java
@@ -1,14 +1,47 @@
 package com.am.upsidedown;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.side_menu_settings:
+                Toast.makeText(getApplicationContext(),"Setting",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.side_menu_logout:
+                Toast.makeText(getApplicationContext(),"Logout"  ,Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.side_menu, menu);
+        return true;
     }
 }
