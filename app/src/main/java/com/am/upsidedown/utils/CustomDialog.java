f2c6fa0461975ab77dd32115003ba4bdc383ef37
Arbena <arbena.musa@student.uni-pr.edu>
2020-05-15 01:23:53 +0200

    Name refactor

--- app/src/main/java/com/am/upsidedown/utils/CustomDialog.java
+++ app/src/main/java/com/am/upsidedown/utils/CustomDialog.java
@@ -2,6 +2,7 @@ package com.am.upsidedown.utils;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.View;
@@ -19,7 +20,7 @@ public class CustomDialog extends Dialog {
         super(context);
     }
 
-    /**
+      /**
      * This method sets the dialog's view content,
      * handles the close button action
      * and returns the dialog object.
@@ -29,12 +30,20 @@ public class CustomDialog extends Dialog {
      */
     public Dialog setLayout(int layout) {
         this.setContentView(layout);
-
         txtclose = (TextView) this.findViewById(R.id.dialog_close);
         txtclose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CustomDialog.super.dismiss();
+                dismiss();
+            }
+        });
+
+        this.setOnCancelListener(new DialogInterface.OnCancelListener()
+        {
+            @Override
+            public void onCancel(DialogInterface dialog)
+            {
+                getOwnerActivity().finish();
             }
         });
 
