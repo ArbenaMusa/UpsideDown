3d86ed55780cd0fb998c70555f437decd087eec5
Arbena <arbena.musa@student.uni-pr.edu>
2020-05-14 10:58:33 +0200

    Added logout functionality on side menu using FirebaseAuth

--- app/src/main/java/com/am/upsidedown/MainActivity.java
+++ app/src/main/java/com/am/upsidedown/MainActivity.java
@@ -3,19 +3,25 @@ package com.am.upsidedown;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
 
@@ -24,21 +30,34 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    /**
+     * This method detects user interaction with the side menu
+     * and provides logout action handler.
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        switch (id){
+        switch (id) {
             case R.id.side_menu_settings:
-                Toast.makeText(getApplicationContext(),"Setting",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Setting", Toast.LENGTH_LONG).show();
                 return true;
             case R.id.side_menu_logout:
-                Toast.makeText(getApplicationContext(),"Logout"  ,Toast.LENGTH_LONG).show();
-                return true;
+                Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_LONG).show();
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this, AuthActivity.class));
+                finish();
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
+    /**
+     * This method inflates side_menu resource.
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.side_menu, menu);
