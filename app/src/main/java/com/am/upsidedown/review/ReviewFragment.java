d1561164d89e2c41f657759cd71148e435d8e84e
MedinaKrelani <medina.krelani@studentet.uni-pr.edu>
2020-06-17 22:44:23 +0200

    Added review_fragment layout and ReviewFragment.java

--- app/src/main/java/com/am/upsidedown/review/ReviewFragment.java
+++ app/src/main/java/com/am/upsidedown/review/ReviewFragment.java
@@ -11,12 +11,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.am.upsidedown.MainActivity;
 import com.am.upsidedown.R;
 
 public class ReviewFragment extends Fragment {
 
     private ReviewViewModel mViewModel;
+
+    RatingBar mRatingBar;
+    TextView mRatingScale;
+    EditText mFeedback;
+    Button mSendFeedback;
+
 
     public static ReviewFragment newInstance() {
         return new ReviewFragment();
@@ -25,14 +37,67 @@ public class ReviewFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.review_fragment, container, false);
+        View view = inflater.inflate(R.layout.review_fragment, container, false);
+
+        mRatingBar = view.findViewById(R.id.ratingBar);
+        mRatingScale = view.findViewById(R.id.tvRatingScale);
+        mFeedback = view.findViewById(R.id.etFeedback);
+        mSendFeedback = view.findViewById(R.id.btnSubmit);
+
+        mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                mRatingScale.setText(String.valueOf(v));
+                switch ((int) ratingBar.getRating()) {
+                    case 1:
+                        mRatingScale.setText("Very bad");
+                        break;
+                    case 2:
+                        mRatingScale.setText("Need some improvement");
+                        break;
+                    case 3:
+                        mRatingScale.setText("Good");
+                        break;
+                    case 4:
+                        mRatingScale.setText("Great");
+                        break;
+                    case 5:
+                        mRatingScale.setText("Awesome. I love it");
+                        break;
+                    default:
+                        mRatingScale.setText("");
+                }
+            }
+        });
+        return view;
+
+
+       /** mSendFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mFeedback.getText().toString().isEmpty()) {
+                    Toast.makeText(ReviewFragment.this, "Please fill in feedback text box", Toast.LENGTH_LONG).show();
+                } else {
+                    mFeedback.setText("");
+                    mRatingBar.setRating(0);
+                    Toast.makeText(ReviewFragment.this, "Thank you for sharing your feedback", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        return view;
+        **/
+
     }
+
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(ReviewViewModel.class);
+
         // TODO: Use the ViewModel
     }
 
 }
+
