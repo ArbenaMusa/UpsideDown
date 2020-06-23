cfa0707a3c5ed629575ef6e239a02a93e3bee0ee
MedinaKrelani <medina.krelani@studentet.uni-pr.edu>
2020-06-23 15:22:36 +0200

    Added Pick an image from gallery and show in ImageView

--- app/src/main/java/com/am/upsidedown/RegisterActivity.java
+++ app/src/main/java/com/am/upsidedown/RegisterActivity.java
@@ -1,9 +1,11 @@
 package com.am.upsidedown;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -30,10 +32,12 @@ import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final int GALLERY_REQUEST_CODE = 123;
     private ImageView userimage;
     private EditText txtName, txtSurname;
     private Spinner userOrWorkman, titleofJob;
     private Button btnRegisterUser;
+    private Button btnPick;
     private String email, password;
     private String name, surname, workmanOrUser, occupation;
 
@@ -54,9 +58,20 @@ public class RegisterActivity extends AppCompatActivity {
         userOrWorkman = findViewById(R.id.spinner1);
         titleofJob = findViewById(R.id.spinner2);
         btnRegisterUser = findViewById(R.id.btnregisterUser);
+        btnPick = findViewById(R.id.btnPickImage);
 
         mAuth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
+
+        btnPick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Pick an image"), GALLERY_REQUEST_CODE);
+            }
+        });
 
         btnRegisterUser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,6 +104,24 @@ public class RegisterActivity extends AppCompatActivity {
 
         addItemsOnSpinner2();
         addListenerOnSpinnerItemSelection();
+    }
+
+    /**
+     * onActivityResult will be called every time we choose an image from gallery
+     * add Uri to store image data
+     * 
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Uri imageData = data.getData();
+
+            userimage.setImageURI(imageData);
+        }
     }
 
     // add items into spinner dynimacally
