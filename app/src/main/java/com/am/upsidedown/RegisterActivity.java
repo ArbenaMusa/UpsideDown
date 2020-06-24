b9047dcf02c89ec0049f713ffcce2f5c80091397
Arbena <arbena.musa@student.uni-pr.edu>
2020-06-24 07:28:00 +0200

    Added profile image storage functions

--- app/src/main/java/com/am/upsidedown/RegisterActivity.java
+++ app/src/main/java/com/am/upsidedown/RegisterActivity.java
@@ -3,13 +3,24 @@ package com.am.upsidedown;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 
+import android.webkit.MimeTypeMap;
 import android.widget.Adapter;
 
 import android.widget.AdapterView;
@@ -29,25 +40,38 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class RegisterActivity extends AppCompatActivity{
+public class RegisterActivity extends AppCompatActivity {
 
     private static final int GALLERY_REQUEST_CODE = 123;
-    private ImageView userimage;
+    public static final int CAMERA_PERM_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
+    private ImageView selectedImage;
+    private String currentPhotoPath;
     private EditText txtName, txtSurname;
     private Spinner role, occupation;
     private Button btnRegisterUser;
-    private Button btnPick;
+    private Button btnPick, btnTake;
     private String name, surname, email;
     private String userRole, userJob;
+    private String filePath;
+    private Uri uri;
 
     private FirebaseAuth mAuth;
     private FirebaseFirestore firestore;
+    private StorageReference storageReference;
     private String userId;
 
     private static final String TAG = "RegisterActivity";
@@ -57,19 +81,21 @@ public class RegisterActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        userimage = findViewById(R.id.image);
+        selectedImage = findViewById(R.id.image);
         txtName = findViewById(R.id.txtname);
         txtSurname = findViewById(R.id.txtsurname);
         role = findViewById(R.id.spinner1);
         occupation = findViewById(R.id.spinner2);
         btnRegisterUser = findViewById(R.id.btnregisterUser);
         btnPick = findViewById(R.id.btnPickImage);
+        btnTake = findViewById(R.id.btnTakeImage);
 
         role = (Spinner) findViewById(R.id.spinner1);
         occupation = (Spinner) findViewById(R.id.spinner2);
 
         mAuth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         btnPick.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,6 +104,13 @@ public class RegisterActivity extends AppCompatActivity{
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent, "Pick an image"), GALLERY_REQUEST_CODE);
+            }
+        });
+
+        btnTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askCameraPermissions();
             }
         });
 
@@ -92,58 +125,48 @@ public class RegisterActivity extends AppCompatActivity{
                 if (getIntent().getExtras() != null) {
                     email = getIntent().getExtras().getString("email");
                 }
-                user.put("name", name);
-                user.put("surname", surname);
-                user.put("email", email);
-                user.put("role", userRole);
-                if (userRole == "Workman") {
-                    user.put("occupation", userJob);
+                if (!name.isEmpty() || !surname.isEmpty()) {
+                    user.put("name", name);
+                    user.put("surname", surname);
+                    user.put("email", email);
+                    user.put("role", userRole);
+                    if (userRole == "Workman") {
+                        user.put("occupation", userJob);
+                    }
+
+                    documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "User profile created for " + userId);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "on failure: " + e.toString());
+                        }
+                    });
+
+                    uploadImageToFirebase(filePath, uri);
+
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "All fields are required!", Toast.LENGTH_SHORT).show();
                 }
-                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "User profile created for " + userId);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d(TAG, "on failure: " + e.toString());
-                    }
-                });
-                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
             }
         });
 
         addItemsOnSpinner2();
+
         addListenerOnSpinnerItemSelection();
+
     }
-
-    /**
-     * onActivityResult will be called every time we choose an image from gallery
-     * add Uri to store image data
-     *
-     * @param requestCode
-     * @param resultCode
-     * @param data
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
-            Uri imageData = data.getData();
-
-            userimage.setImageURI(imageData);
-        }
-    }
-
 
     /**
      * This method adds dynamically spinner2 when an item in spinner1 is selected
-     *
      */
-    public void addItemsOnSpinner2(){
+    public void addItemsOnSpinner2() {
         List<String> list = new ArrayList<String>();
         list.add("Electrician");
         list.add("Plumber");
@@ -183,5 +206,135 @@ public class RegisterActivity extends AppCompatActivity{
 
             }
         });
+    }
+
+    /**
+     * This methods saves user's image on firebase storage.
+     *
+     * @param name
+     * @param contentUri
+     */
+    private void uploadImageToFirebase(String name, Uri contentUri) {
+        final StorageReference image = storageReference.child("pictures/" + name);
+        image.putFile(contentUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                image.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Log.d("tag", "onSuccess: Uploaded Image URl is " + uri.toString());
+                    }
+                });
+
+                Toast.makeText(RegisterActivity.this, "Image Is Uploaded.", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegisterActivity.this, "Upload Failled.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    /**
+     * onActivityResult will be called every time we choose an image from gallery
+     * add Uri to store image data
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                File f = new File(currentPhotoPath);
+                filePath = f.getName();
+                uri = Uri.fromFile(f);
+                selectedImage.setImageURI(Uri.fromFile(f));
+                Log.d("tag", "Absolute Url of Image is " + Uri.fromFile(f));
+
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri);
+                mediaScanIntent.setData(uri);
+                this.sendBroadcast(mediaScanIntent);
+            }
+        }
+
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                uri = data.getData();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                filePath = "JPEG_" + timeStamp + "." + getFileExt(uri);
+                Log.d("tag", "onActivityResult: Gallery Image Uri:  " + filePath);
+                selectedImage.setImageURI(uri);
+            }
+        }
+    }
+
+    private void askCameraPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        } else {
+            dispatchTakePictureIntent();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CAMERA_PERM_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent();
+            } else {
+                Toast.makeText(this, "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private String getFileExt(Uri contentUri) {
+        ContentResolver c = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "net.smallacademy.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
     }
 }
